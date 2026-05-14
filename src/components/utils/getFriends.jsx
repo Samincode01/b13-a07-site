729fcd98c66f529e88e5256dd68fdb815af87ff7
Samincode01 <samin990bd@gmail.com>
2026-05-14 23:44:00 +0600

    Added api

--- src/components/utils/getFriends.jsx
+++ src/components/utils/getFriends.jsx
@@ -1,9 +1,7 @@
 
 const getFriends = async() => {
-    // https://6a0606e4c83ba8ad9b3d2608.mockapi.io/friends
-    const res = await fetch("http://localhost:3000/friends.json");
-    const data = await res.json();
-
+    const res = await fetch('/api/friends')
+    const data = await res.json()
     return data;
 };
 
