ac398a2ebbb61051331e8c84f28a06dd2363eefa
Samincode01 <samin990bd@gmail.com>
2026-05-14 23:51:42 +0600

    Fixed api issue

--- src/components/utils/getFriends.jsx
+++ src/components/utils/getFriends.jsx
@@ -1,7 +1,9 @@
 
 const getFriends = async() => {
-    const res = await fetch('/api/friends')
-    const data = await res.json()
+ 
+    const res = await fetch("https://6a0606e4c83ba8ad9b3d2608.mockapi.io/friends");
+    const data = await res.json();
+
     return data;
 };
 
