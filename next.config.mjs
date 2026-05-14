67053ae071f169030ee8894cb95e00bcdc687870
Samincode01 <samin990bd@gmail.com>
2026-05-15 02:36:46 +0600

    Fixed api and export issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: "export",
+
   images: {
-    domains: ['images.pexels.com'],
+    domains: ["images.pexels.com"],
+    unoptimized: true,
   },
 };
 
--- src/app/friend/[id]/page.jsx
+++ src/app/friend/[id]/page.jsx
@@ -2,13 +2,25 @@ import CheckInBtn from "@/components/FriendDetailsPage/CheckInBtn";
 import RightSide from "@/components/FriendDetailsPage/RightSide";
 import BannerCard from "@/components/ui/BannerCard";
 import getFriends from "@/components/utils/getFriends";
+import friends from "@/data/friends.json";
+
 import React from "react";
 import { LuMessageSquareText, LuPhone, LuVideo } from "react-icons/lu";
 
+export async function generateStaticParams() {
+  return friends.map((friend) => ({
+    id: friend.id.toString(),
+  }));
+}
+
 const FriendDetailsPage = async ({ params }) => {
   const { id } = await params;
-  const friends = await getFriends();
-  const friend = friends.find((fd) => fd.id === Number(id));
+
+  const friendsData = await getFriends();
+
+  const friend = friendsData.find(
+    (fd) => fd.id === Number(id)
+  );
 
   const rightTopCardItems = [
     {
@@ -32,19 +44,30 @@ const FriendDetailsPage = async ({ params }) => {
   ];
 
   return (
-    <div className="max-w-325  w-[95%] sm:w-10/12 mx-auto grid lg:grid-cols-3 my-10 gap-5">
+    <div className="max-w-325 w-[95%] sm:w-10/12 mx-auto grid lg:grid-cols-3 my-10 gap-5">
+      
       <div className="lg:col-span-1">
-        <RightSide friend={friend}></RightSide>
+        <RightSide friend={friend} />
       </div>
+
       <div className="lg:col-span-2">
+        
         <div className="grid sm:grid-cols-3 gap-3">
           {rightTopCardItems.map((item, ind) => (
-            <BannerCard key={ind} num={item.num} text={item.text}></BannerCard>
+            <BannerCard
+              key={ind}
+              num={item.num}
+              text={item.text}
+            />
           ))}
         </div>
+
         <div className="p-4 sm:p-6 shadow-sm bg-white rounded-xl mt-5 flex flex-col min-[400px]:flex-row justify-between gap-5">
+          
           <div className="space-y-7">
-            <h2 className="text-[#2d4a43] font-bold text-center min-[400px]:text-start">Relationship Goal</h2>
+            <h2 className="text-[#2d4a43] font-bold text-center min-[400px]:text-start">
+              Relationship Goal
+            </h2>
 
             <p className="text-gray-500 text-center min-[400px]:text-start">
               Connect every{" "}
@@ -53,16 +76,28 @@ const FriendDetailsPage = async ({ params }) => {
               </span>
             </p>
           </div>
-          <button className="btn">Edit</button>
-        </div>
-        <div className="space-y-4 mt-6 p-5 shadow-sm bg-white rounded-2xl">
-          <h2 className="text-[#2d4a43] font-bold text-lg">Quick Check-In</h2>
 
-          <div className="grid min-[450px]:grid-cols-3 gap-4 ">
+          <button className="btn">
+            Edit
+          </button>
+        </div>
+
+        <div className="space-y-4 mt-6 p-5 shadow-sm bg-white rounded-2xl">
+          
+          <h2 className="text-[#2d4a43] font-bold text-lg">
+            Quick Check-In
+          </h2>
+
+          <div className="grid min-[450px]:grid-cols-3 gap-4">
             {checkInOptions.map((item, ind) => (
-              <CheckInBtn key={ind} item={item} friend={friend}></CheckInBtn>
+              <CheckInBtn
+                key={ind}
+                item={item}
+                friend={friend}
+              />
             ))}
           </div>
+
         </div>
       </div>
     </div>
